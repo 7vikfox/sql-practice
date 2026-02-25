e1b6774d61eb693bfe4a7afd73b2228bb4c18541
7vikfox <miw6515@gmail.com>
2026-02-25 04:08:33 -0800

    Add SQL query for highest salary in each department
    This SQL query retrieves employees with the highest salary in each department, joining the Employee and Department tables.

---
+++ leetcode-practice/027_Employee_HighestSalary_Department.sql
@@ -0,0 +1,51 @@
+/*
+LeetCode SQL Practice
+Problem: Highest Salary in Each Department
+
+Description:
+Find employees who have the highest salary in each department.
+
+Tables:
+Employee(id, name, salary, departmentId)
+Department(id, name)
+
+Example:
+Input:
+Employee:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+
+Department:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+
+Output:
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+
+*/
+
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
