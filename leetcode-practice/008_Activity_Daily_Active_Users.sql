fe94058218a0623697c5948e9cd6b3635dacfb1e
7vikfox <miw6515@gmail.com>
2026-02-25 03:44:43 -0800

    Add SQL query for daily active user count
    This SQL query calculates the daily active user count over a 30-day period ending on 2019-07-27, based on user activities recorded in the Activity table.

---
+++ leetcode-practice/008_Activity_Daily_Active_Users.sql
@@ -0,0 +1,42 @@
+/*
+LeetCode SQL Practice
+Problem: Daily Active Users
+
+Description:
+Find the daily active user count for a period of 30 days ending 2019-07-27 (inclusive). 
+A user is active on a day if they have at least one activity on that day.
+
+Tables:
+Activity(user_id, session_id, activity_date, activity_type)
+
+Example:
+Input:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
++---------+------------+---------------+---------------+
+
+Output:
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+
+*/
+
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY activity_date;
